e0277714c60ba39c3593466624b87121ea5702a8
zayelmech <i.equipo001@gmail.com>
2021-04-10 22:39:51 -0500

    JSX en accion clase 12

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 //const element = document.createElement('h1');
 //element.innerText = 'Hello_, Platzi Badges !';
-
 //const container = document.getElementById('app');
-
 //container.appendChild(element);
 
 
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello,Platzi Badges from React! </h1>;
+
+//const element= React.createElement('a',{href:'https://www.imecatro.com'},'Hola soy los children');
+
+
+const nombreFunction = ()=>{
+    const name = 'Abdiel';
+    return name;
+}
+const elementReact = React.createElement('h2',{},`Hola, soy un h2 incrustado  ${1}`);
+const elementJSX =
+<div>
+    <h1>Hello,Platzi Badges from React! {2+2}</h1>
+    <p>Mi nombre es {nombreFunction()} </p>
+    {elementReact}
+</div>;
+//El profe no recomienda usar React Element, Es mejor usar JSX forever 
 const container =document.getElementById('app');
 
 
-ReactDOM.render(element, container);
+ReactDOM.render(elementJSX, container);
