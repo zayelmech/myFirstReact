44c9bb3f591c03bdce3fe5b7cab434c317963590
zayelmech <i.equipo001@gmail.com>
2021-04-10 22:08:54 -0500

    ReactDOM.render clase 6

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//const element = document.createElement('h1');
+//element.innerText = 'Hello_, Platzi Badges !';
+
+//const container = document.getElementById('app');
+
+//container.appendChild(element);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const element = <h1>Hello,Platzi Badges from React! </h1>;
+const container =document.getElementById('app');
+
+
+ReactDOM.render(element, container);
